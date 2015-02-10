98890491e2d2d5fb5a35e5a890351fb31221f0d0
Lisa Passing <mail@lislis.de>
2015-02-10 18:22:02 +0100

    change markup and styles

--- charmeur.js
+++ charmeur.js
@@ -4,94 +4,72 @@
 (function(){
   var tab, box, email, shown = false, sending = false, openmsg = null, callbacks = {},
     VENDORS = ['webkit','moz','o','ms'],
-    RESET = 'margin:0;padding:0;border:0;outline:0;font-weight:inherit;text-align:left;font-style:inherit;letter-spacing:inherit;background:transparent;'+
-      'top:auto;left:auto;right:auto;bottom:auto;'+
-      'font-size:100%;font-family:inherit;vertical-align:baseline;list-style:none;inherit;'+vendored('box-sizing','inherit')+vendored('box-shadow','none')+
-      'color:inherit;line-height:1;font:14px/18px Arial;border-radius:0;-webkit-font-smoothing:subpixel-antialiased;text-rendering:auto;',
     STYLE =
-      '@media screen {\n'+
-      '#CHARM_TAB { '+RESET+'position:fixed;z-index:999999;left:-10px;cursor:pointer;top:50%;margin-top:-100px;width:40px;height:200px;color:#fff;'+
-        ';background:url(https://secure.charmhq.com/feedback.png);background-size:100% 100%;'+
-        vendored('transform','translateX(0)') + vendored('transition','all 0.4s') + '}\n'+
-      '#CHARM_TAB:hover { '+vendored('transform','translateX(10px)')+' }\n'+
-      '#CHARM_TAB.hidden {'+vendored('transform','translateX(-40px)')+'}\n'+
-      '#CHARM_TAB.hidden:hover { '+vendored('transform','translateX(-40px)')+'}\n'+
-      '#CHARM_BOX * { '+RESET+' }\n'+
-      '#CHARM_BOX * :focus { outline:0; }\n'+
-      '#CHARM_BOX b { font-weight:bold; }\n'+
-      '#CHARM_BOX iframe { width:1px;height:1px;visibility:hidden;position:absolute; }\n'+
-      '#CHARM_BOX, #CHARM_MESSAGE { '+RESET+
-        'border:8px solid #fff;'+
-        'position:fixed;'+
-        'z-index:999999;left:50%;top:50%;margin-top:-148px;margin-left:-218px;'+
-        'width:400px;'+
-        'background:#35aec6;'+
-        'background:-webkit-gradient(linear, left top, left bottom, from(#56c6dc), to(#35aec6));'+
-        'background:-moz-linear-gradient(center bottom, #35aec6 0%, #56c6dc 100%);'+
-        'text-shadow:0px 1px 0px rgba(0,0,0,0.15);padding:10px;color:#fff;'+
-        'opacity:0;'+
-        vendored('border-radius','9px')+
-        vendored('box-shadow','0px 0px 20px rgba(0,0,0,0.3)')+
-        vendored('transform','scale(1.5)')+
-        vendored('transition', 'all 0.45s')+
-      '}\n'+
-      '#CHARM_MESSAGE { '+
-        'height: auto; text-align:center; font:18px/22px Arial; margin-top: -60px; '+
-      '}\n'+
-      '#CHARM_YOUR_EMAIL { margin-top: 10px; }\n'+
-      '#CHARM_EMAIL { display:block;margin:5px 0 9px 0;padding:8px;width:384px;height:20px;color:#000;background:#fff; '+
-        vendored('box-shadow','inset 0px -2px 5px 5px rgba(0,0,0,0.03)')+
-        vendored('border-radius','4px')+
-      '}\n'+
-      '#CHARM_YOUR_COMMENT { margin-top: 5px; }\n'+
-      '#CHARM_COMMENT { margin:5px 0 9px 0;padding:8px;width:384px;height:104px;color:#000;background:#fff;resize:none; '+
-        vendored('box-shadow','inset 0px -2px 5px 5px rgba(0,0,0,0.03)')+
-        vendored('border-radius','4px')+
-      '}\n'+
-      '#CHARM_SUBMIT { width:auto;border:1px solid #fff;padding:4px 10px;'+
-        'background:#fff;color:#000;font-weight:bold;'+vendored('border-radius','9px')+'cursor:pointer;text-shadow:0px 1px 0px #fff;'+
-        'background:-webkit-gradient(linear, left top, left bottom, from(#fff), to(#ddd));'+
-        'background:-moz-linear-gradient(center bottom, #ddd 0%, #fff 100%);'+
-        'display:inherit;'+
-        vendored('box-shadow','0px 1px 2px rgba(0,0,0,0.5)')+
-      '}\n'+
-      '#CHARM_SUBMIT:hover { background:#eee; }\n'+
-      'a#CHARM_CANCEL { position:absolute;bottom:15px;right:12px;text-decoration:underline;color:#fff; }\n'+
-      'a#CHARM_CANCEL:hover { color:#fff; }\n'+
-      'a#CHARM_CANCEL, a#CHARM_CANCEL:hover { display:block;float:right;margin-top:7px;font-size:12px; }\n'+
-      '#CHARM_BOX.open, #CHARM_MESSAGE.open { '+vendored('transform','scale(1)')+'opacity:1; }\n'+
-      '#CHARM_BOX.closed, #CHARM_MESSAGE.closed { '+
-        'opacity:0;'+
-        vendored('transform','scale(1.5)')+
-        vendored('transition', 'all 0.25s')+
-      '}\n'+
-      '#CHARM_MESSAGE { text-align:center;height:auto; }\n'+
-      '* html #CHARM_TAB { position: absolute; }\n'+
-      '* html #CHARM_TAB.hidden { display: none; }\n'+
-      '* html #CHARM_SUBMIT { background:#fff; }\n'+      
-      '*+html #CHARM_SUBMIT { background:#fff; }\n'+      
-      '* html #CHARM_BOX { background:#35aec6; position:absolute; }\n'+
-      '*+html #CHARM_BOX { background:#35aec6; }\n'+
-      '* html #CHARM_MESSAGE { background:#35aec6; position:absolute; }\n'+
-      '*+html #CHARM_MESSAGE { background:#35aec6; }\n'+
-      '}\n'+
-      '@media print {\n'+
-      '#CHARM_TAB, #CHARM_BOX, #CHARM_MESSAGE { display:none !important; }\n'+
-      '}\n'+
-      '@media only screen and (-moz-min-device-pixel-ratio: 1.5), only screen and (-o-min-device-pixel-ratio: 3/2), only screen and (-webkit-min-device-pixel-ratio: 1.5), only screen and (min-device-pixel-ratio: 1.5) { #CHARM_TAB { background-image:url(https://secure.charmhq.com/feedback2x.png); }}',
+      '.feedback-popup {' +
+      '    position: fixed;' +
+      '    top: 50%;' +
+      '    left: 50%;' +
+      '    width: 30em;' +
+      '    transform: translate(-50%, -50%);' +
+      '    padding: 1em 1.3em;' +
+      '    background-color: #ffffff;' +
+      '    border-radius: 5px;' +
+      '    box-shadow: 0px 0px 3px 1px rgba(0,0,0,0.15);' +
+      '  }' +
+      '  .feedback-popup h1 {' +
+      '    margin: 0 0 .4em;' +
+      '    padding-left: .4em;' +
+      '    color: #347389;' +
+      '    font-size: 20px;' +
+      '  }' +
+      '  .feedback-popup textarea {' +
+      '    box-sizing: border-box;' +
+      '    display: block;' +
+      '    width: 100%;' +
+      '    height: 10em;' +
+      '    margin-bottom: .8em;' +
+      '    padding: .5em .6em;' +
+      '    resize: vertical;' +
+      '    background-color: #f4f3eb;' +
+      '    font-size: 15px;' +
+      '    color: #7d7e80;' +
+      '    border: none;' +
+      '    font-family: "Source Sans Pro", Helvetica, sans-serif;' +
+      '  }' +
+      '  .feedback-popup .submit {' +
+      '    margin-right: 1em;' +
+      '    padding: .3em .7em;' +
+      '    background-color: #37a766;' +
+      '    color: #ffffff;' +
+      '    font-size: 15px;' +
+      '    border: none;' +
+      '    border-radius: 4px;' +
+      '    font-family: "Source Sans Pro", Helvetica, sans-serif;' +
+      '  }' +
+      '  .feedback-popup a {' +
+      '    color: #7d7e80;' +
+      '    font-size: 15px;' +
+      '    text-decoration: none;' +
+      '  }' +
+      '  .feedback-popup a:hover {' +
+      '    text-decoration: underline;' +
+      '  }',
     BOX =
-      '<span id="CHARM_TEXT"></span>'+
-      '<iframe id="CHARM_FORM_TARGET" name="CHARM_FORM_TARGET" src="javascript:void(0)" onload="__CHARM&&__CHARM.iFrameLoaded&&__CHARM.iFrameLoaded()" onerror="__CHARM&&__CHARM.iFrameError&&__CHARM.iFrameError()"></iframe>'+
-      '<form id="CHARM_FORM" target="CHARM_FORM_TARGET" action="https://secure.charmhq.com/feedback" method="POST">'+
+      '<div class="feedback-popup">' +
+      '<h1>Have feedback or questions?</h1>' +
+      '<iframe id="CHARM_FORM_TARGET" name="CHARM_FORM_TARGET" src="javascript:void(0)" onload="__CHARM&&__CHARM.iFrameLoaded&&__CHARM.iFrameLoaded()" onerror="__CHARM&&__CHARM.iFrameError&&__CHARM.iFrameError()"></iframe>' +
+      '<form id="CHARM_FORM" action="https://secure.charmhq.com/feedback" method="POST" accept-charset="utf-8">' +
       '<div id="CHARM_YOUR_EMAIL"></div>'+
       '<div id="CHARM_YOUR_COMMENT"></div>'+
-      '<textarea id="CHARM_COMMENT" name="content" class="ignore-return-pressed"></textarea>'+
-      '<input id="CHARM_SUBMIT" type="submit" value=""> <a href="#" id="CHARM_CANCEL"></a>'+
-      '</form>',
+      '<textarea id="CHARM_COMMENT" name="content" class="ignore-return-pressed" placeholder="Let us know and we will get right back to you by email"></textarea>' +
+      '<input id="CHARM_SUBMIT" type="submit" class="submit" value="">' +
+      '<a href="#" title="" id="CHARM_CANCEL"></a>' +
+      '</form>' +
+      '</div>',
     DEFAULTS = {
-      text: '<b>Do you have feedback or questions?</b><br/> Let us know and we will get right back to you by email.',
+      text: '',
       submit: 'Send feedback',
-      cancel: 'hide this',
+      cancel: 'cancel',
       your_email: 'Your email address:',
       your_comment: 'Your message:',
       feedback_sending: 'Sending...',
